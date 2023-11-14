03a59516b1ca176e1365c0dbd5363dd01dd2ca62
Euro Abo <abaoeuro2002@gmail.com>
2023-11-15 04:11:32 +0800

    bug fixed

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/auth", require("./routes/userRoutes"));
 app.use("/api/message", require("./routes/messageRoute"));
-a;
+
 const io = new Server(server, {
   cors: {
     origin: "https://flap-chat.vercel.app",
